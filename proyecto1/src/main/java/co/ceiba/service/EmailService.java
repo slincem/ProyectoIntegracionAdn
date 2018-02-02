94d4de04cc770ec741fb00a60d391cc9762fd8a7
slincem <slincem@gmail.com>
2018-02-02 09:05:36 -0500

    Revert "Revert "Primer Commit del proyecto""
    This reverts commit 438d37ebec3d0dcdf10c1563ac0a7e170672e013.

---
+++ proyecto1/src/main/java/co/ceiba/service/EmailService.java
@@ -0,0 +1,9 @@
+package co.ceiba.service;
+
+public class EmailService {
+	
+	public String sendEmail(String message) {
+		return "Email send sucessful with message " + message;
+	}
+
+}
---
+++ proyecto1/src/test/java/co/ceiba/service/NotifyServiceTest.java
@@ -0,0 +1,32 @@
+package co.ceiba.service;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import co.ceiba.domain.Person;
+import co.ceiba.testdatabuilder.PersonTestDataBuilder;
+
+public class NotifyServiceTest {
+	
+	private NotifyPersonService notifyPersonService;
+	private EmailService emailService;
+	
+	@Before
+	public void setup() {
+		emailService = Mockito.mock(EmailService.class);
+		notifyPersonService = new NotifyPersonService(emailService);
+	}
+	
+	@Test
+	public void notifyTest() {
+		//Arrange
+		Person person = new PersonTestDataBuilder().build();
+		Mockito.when(emailService.sendEmail(Mockito.anyString())).thenReturn("Hola Mundo");
+		//Act
+		String message = notifyPersonService.notify(person);
+		//Assert
+		Assert.assertNotNull(message);
+	}
+}
